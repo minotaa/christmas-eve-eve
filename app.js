dc92a0f6d9b20c0f2a75c6076cb4c92a0a9820e2
Minota <34592028+xMinota@users.noreply.github.com>
2019-11-21 12:12:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ function doIt() {
   const one_day=1000*60*60*24;
   var str = "Eve "
   var s1 = str.repeat(Math.ceil((cmas.getTime()-today.getTime())/(one_day)))
-  document.getElementById("ever") = "It is Christmas" + s1;
+  document.getElementById("ever").innerHTML = "It is Christmas " + s1;
 }
