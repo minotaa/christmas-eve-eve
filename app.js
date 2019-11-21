35fdd34ca12fbb7df0066b0bd72dd65806fcd1c5
Minota <34592028+xMinota@users.noreply.github.com>
2019-11-21 12:23:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,34 @@ function doIt() {
   var s1 = str.repeat(Math.ceil((cmas.getTime()-today.getTime())/(one_day)))
   document.getElementById("ever").innerHTML = "It is Christmas " + s1;
 }
+
+function calculateChristmasCountdown() {
+    var now = new Date();
+    var currentMonth = (now.getMonth() + 1);
+    var currentDay = now.getDate();
+    var nextChristmasYear = now.getFullYear();
+    if(currentMonth == 12 && currentDay > 25){
+        nextChristmasYear = nextChristmasYear + 1;
+    }
+    var nextChristmasDate = nextChristmasYear + '-12-25T00:00:00.000Z';
+    var christmasDay = new Date(nextChristmasDate);
+    var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
+    if(currentMonth != 12 || (currentMonth == 12 && currentDay != 25)){
+        days = Math.floor(diffSeconds / (3600*24));
+        diffSeconds  -= days * 3600 * 24;
+        hours   = Math.floor(diffSeconds / 3600);
+        diffSeconds  -= hours * 3600;
+        minutes = Math.floor(diffSeconds / 60);
+        diffSeconds  -= minutes * 60;
+        seconds = diffSeconds;
+    }
+    document.getElementById('days').innerHTML = days + ' Days';
+    document.getElementById('hours').innerHTML = hours + ' Hours';
+    document.getElementById('minutes').innerHTML = minutes + ' Minutes';
+    document.getElementById('seconds').innerHTML = seconds + ' Seconds';
+    setTimeout(calculateChristmasCountdown, 1000);
+}
