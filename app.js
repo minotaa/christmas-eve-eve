9f8c45a64da85df0242c775649e54823e1cd0fbd
Minota <34592028+xMinota@users.noreply.github.com>
2019-11-21 12:06:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-today=new Date();
-const cmas=new Date(today.getFullYear(), 11, 25);
-if (today.getMonth()==11 && today.getDate()>25) 
-{
-cmas.setFullYear(cmas.getFullYear()+1); 
-}  
-const one_day=1000*60*60*24;
-document.getElementById("ever") = `${Math.ceil((cmas.getTime()-today.getTime())/(one_day))} days left until Christmas!`;
+function start() {
+  setInterval(doIt, 3000);
+}
+
+function doIt() {
+  
+  today=new Date();
+  const cmas=new Date(today.getFullYear(), 11, 25);
+  if (today.getMonth()==11 && today.getDate()>25) 
+  {
+  cmas.setFullYear(cmas.getFullYear()+1); 
+  }  
+  const one_day=1000*60*60*24;
+  var str = "Eve "
+  var s1 = str.repeat(Math.ceil((cmas.getTime()-today.getTime())/(one_day)))
+  document.getElementById("ever") = "It is Christmas" + s1;
+}
